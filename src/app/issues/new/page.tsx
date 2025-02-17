efc950f866d623c2045b039097a524ab25903b1f
sh3rwin21 <sherwin@3enix.com>
2025-02-18 04:24:08 +0800

    handle error

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
-import { useForm, Controller } from "react-hook-form";
-import { useRef } from "react";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
+import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
+import { CiWarning } from "react-icons/ci";
 
 interface IssueForm {
   title: string;
@@ -17,21 +18,38 @@ const NewIssuePage = () => {
   const { register, control, handleSubmit } = useForm<IssueForm>();
   const titleRef = useRef(register("title"));
 
+  const [error, setError] = useState("");
+
   const handleIssueSubmit = async (data: IssueForm) => {
-    await axios.post("/api/issues", data);
-    router.push("/issues");
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (err) {
+      // let error = err as Error & AxiosError;
+      setError("An unexpected error occurred");
+    }
   };
   return (
-    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(handleIssueSubmit)}>
-      <TextField.Root placeholder="Title" {...titleRef.current} />
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
-      />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon>
+            <CiWarning />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className=" space-y-3" onSubmit={handleSubmit(handleIssueSubmit)}>
+        <TextField.Root placeholder="Title" {...titleRef.current} />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+        />
 
-      <Button className="cursor-pointer">Submit New Issue</Button>
-    </form>
+        <Button className="cursor-pointer">Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
